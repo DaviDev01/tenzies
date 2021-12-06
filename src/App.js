27e9f207a5ab17594ef32806f5acc3e8664bbfce
DaviDev01 <davimendesdafonseca@outlook.com>
2021-12-06 18:51:11 -0300

    detals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react"
 import Die from "./components/Die"
 import Confetti from "react-confetti"
+import {nanoid} from "nanoid"
 
 export default function App() {
     const [diceObjs, setDiceObjs] = useState(getData())  
     const [diceObjsPlayer2, setDiceObjsPlayer2] = useState(getData())  
     const [hasWon, setHasWon] = useState(false)
     const [isConfettiTime, setIsConfettiTime] = useState(false)
-    const [twoPlayerMode, setTwoPlayerMode] = useState(true)
+    const [twoPlayerMode, setTwoPlayerMode] = useState(false)
     const [player1Turn, setPlayer1Turn] = useState(true)
-    
+    //const numOfDice = 10
     const diceElements = diceObjs.map( (obj) => {
 
         return <Die key={obj.id} obj={obj} hold={hold} hasWon={hasWon} player1Turn={!player1Turn}/>
@@ -18,7 +19,7 @@ export default function App() {
 
     const diceElementsPlayer2 = diceObjsPlayer2.map( (obj) => {
 
-        return <Die key={obj.id} obj={obj} hold={holdPlayer2} hasWon={hasWon} player1Turn={player1Turn}/>
+        return <Die key={obj.id} obj={obj} hold={hold} hasWon={hasWon} player1Turn={player1Turn}/>
 
     })
 
@@ -27,27 +28,28 @@ export default function App() {
     }
 
     function getData() {
-        const arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        let numOfDice = 10
+        const arrayNum = []
 
-        const objArray = arrayNum.map( (id) => ( { 
-                id: id,
-                value: getRandNum(),
-                isSelected: false
-            })
-        )
+        for (let i = 1; i <= numOfDice; i++ ) {
+            arrayNum.push(
+                { 
+                    id: nanoid(),
+                    value: getRandNum(),
+                    isSelected: false
+                }
+            ) 
+        }
 
-        return objArray
+        return arrayNum
     }
 
-    function roll() {
+    function roll(player) {
         hasWon ? resetGame() :
+        player === "player1" ?
         setDiceObjs(prev => {
             return prev.map(obj => obj.isSelected ? obj : {...obj, value: getRandNum()})
-        })
-    }
-
-    function rollPlayer2() {
-        hasWon ? resetGame() :
+        }) : 
         setDiceObjsPlayer2(prev => {
             return prev.map(obj => obj.isSelected ? obj : {...obj, value: getRandNum()})
         })
@@ -60,26 +62,21 @@ export default function App() {
         setIsConfettiTime(false)
     }
 
-    useEffect( checkForWin, [diceObjs] )
+    useEffect( checkForWin, [diceObjs, diceObjsPlayer2] )
 
     function checkForWin() {
         const isAllSame = diceObjs.filter( obj => obj.value  === diceObjs[0].value && obj.isSelected && obj)
-        
+        const isAllSamePlayer2 = diceObjsPlayer2.filter( obj => obj.value  === diceObjsPlayer2[0].value && obj.isSelected && obj)
+
         if (isAllSame.length === diceObjs.length) {
             setHasWon(true);
             setIsConfettiTime(true);
-        } 
-    }
-
-    useEffect( checkForWin2, [diceObjsPlayer2] )
-    
-    function checkForWin2() {
-        const isAllSame = diceObjsPlayer2.filter( obj => obj.value  === diceObjsPlayer2[0].value && obj.isSelected && obj)
-        
-        if (isAllSame.length === diceObjsPlayer2.length) {
+            console.log("player1 wins")
+        } else if (isAllSamePlayer2.length === diceObjsPlayer2.length) {
             setHasWon(true);
-            setIsConfettiTime(true);
-        } 
+            setIsConfettiTime(true)
+            console.log("player2 wins")
+        }
     }
 
     console.log(isConfettiTime)
@@ -87,67 +84,94 @@ export default function App() {
         const updatedArray = diceObjs.map( (obj) => obj.id === id ? {...obj, isSelected: !obj.isSelected} : obj)
 
         setDiceObjs(updatedArray)
-    }
 
-    function holdPlayer2(id, e) {
-        const updatedArray = diceObjsPlayer2.map( (obj) => obj.id === id ? {...obj, isSelected: !obj.isSelected} : obj)
+        const updatedArray2 = diceObjsPlayer2.map( (obj) => obj.id === id ? {...obj, isSelected: !obj.isSelected} : obj)
 
-        setDiceObjsPlayer2(updatedArray)
+        setDiceObjsPlayer2(updatedArray2)
     }
     
     function lockClick(player) {
         if (player === "player1") {
             setPlayer1Turn(false) 
-            rollPlayer2()
+            roll(player)
         } else {
             setPlayer1Turn(true)
-            roll()
+            roll(player)
         }
     }
 
     isConfettiTime && setTimeout(() => setIsConfettiTime(false), 3740)
 
     return (
-        <main className={`diceContainer ${player1Turn ? "player1Turn" : "player2Turn"}`}>
-            {isConfettiTime && <Confetti />} 
-            {twoPlayerMode ? 
-                <div className="btns-container">
-                    <button 
-                        disabled={player1Turn}
-                        className="lockBtn" 
-                        onClick={() => lockClick("player2")}
+        <div className="outerContainer">
+            <div className="mode">
+                <div className="btns">
+                    <h3 className="mode--title">Mode</h3>
+                    <span 
+                        onClick={() => {
+                        setTwoPlayerMode(true) 
+                        resetGame()}} 
+                        className="menu-btn"
                     >
-                        <img src="../imgs/lock-white.png" alt="lock" className="lock"/>
-                    </button> 
-                </div> : 
-                <h1 className="diceContainer--title">Tenzies</h1>}
-            {twoPlayerMode ?
-                <div className="dicePlayer2">
-                    {diceElementsPlayer2}
-                </div> : 
-            <p className="diceContainer--instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
+                            Two Players Mode
+                    </span>
+                    <span onClick={() => {
+                        setTwoPlayerMode(false)
+                        resetGame()
+                        setPlayer1Turn(true)}} 
+                        className="menu-btn"
+                    >
+                        One Player Mode
+                    </span>
+                </div>
+                {twoPlayerMode && <p className="diceContainer--instructions mode-instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
+            </div>
+            <main className={`diceContainer ${player1Turn ? "player1Turn" : "player2Turn"}`}>
+                {isConfettiTime && <Confetti />} 
+                {twoPlayerMode ? 
+                    <div className="btns-container">
+                        <button 
+                            disabled={player1Turn}
+                            className="lockBtn" 
+                            onClick={() => lockClick("player2")}
+                        >
+                            <img src={`../imgs/${!player1Turn ? 'unlock' : 'lock'}-white.png`} alt="lock" className="lock"/>
+                        </button> 
+                    </div> : 
+                    <h1 className="diceContainer--title">
+                        Tenzies
+                    </h1>}
+                {twoPlayerMode ?
+                    <div className="dicePlayer2">
+                        {diceElementsPlayer2}
+                    </div> : 
+                <p className="diceContainer--instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
 
-            <div className="dice">
-                {diceElements}
+                <div className="dice">
+                    {diceElements}
+                </div>
+                <div className="btns-container">
+                    {!twoPlayerMode && <button
+                        className="diceContainer--btn" 
+                        onClick={() => roll('player1')}
+                    >
+                        {hasWon ? 'Play Again' : 'Roll'}
+                    </button>} 
+                    {
+                    twoPlayerMode && 
+                    <button 
+                        disabled={!player1Turn}
+                        className="lockBtnBlue"
+                        onClick={() => lockClick("player1")}
+                    >
+                        <img src={`../imgs/${player1Turn ? 'unlock' : 'lock'}-white.png`} alt="lock" className="lock"/>
+                    </button>
+                    }
+                </div>
+            </main>
+            <div className="mode hide">
+                
             </div>
-            <div className="btns-container">
-                {!twoPlayerMode && <button
-                    className="diceContainer--btn" 
-                    onClick={roll}
-                >
-                    {hasWon ? 'Play Again' : 'Roll'}
-                </button>} 
-                {
-                twoPlayerMode && 
-                <button 
-                    disabled={!player1Turn}
-                    className="lockBtnBlue"
-                    onClick={() => lockClick("player1")}
-                >
-                    <img src="../imgs/lock-white.png" alt="lock" className="lock"/>
-                </button>
-                }
-            </div>
-        </main>
+        </div>
     )
 }
